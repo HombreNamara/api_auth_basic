c01a71e27426f8ba4f3f6aca6b97d0fc714323e1
HombreNamara <102432184+HombreNamara@users.noreply.github.com>
2024-07-14 22:03:05 -0400

    Add files via upload

--- services/UserService.js
+++ services/UserService.js
@@ -102,9 +102,114 @@ const deleteUser = async (id) => {
     };
 }
 
+const getAllUsers = async () => {
+    const users = await db.User.findAll({
+        where: {
+            status: true
+        }
+    });
+    return {
+        code: 200,
+        message: users
+    };
+};
+
+const findUsers = async (req) => {
+    const { name, eliminado, lastLoginBefore, lastLoginAfter } = req.query;
+    let where = {};
+
+    if (eliminado !== undefined) {
+        where.status = eliminado === 'true' ? false : true;
+    }
+    if (name) {
+        where.name = {
+            [db.Sequelize.Op.like]: `%${name}%`
+        };
+    }
+    if (lastLoginBefore) {
+        where.updatedAt = {
+            [db.Sequelize.Op.lt]: new Date(lastLoginBefore)
+        };
+    }
+    if (lastLoginAfter) {
+        if (where.updatedAt) {
+            where.updatedAt[db.Sequelize.Op.gt] = new Date(lastLoginAfter);
+        } else {
+            where.updatedAt = {
+                [db.Sequelize.Op.gt]: new Date(lastLoginAfter)
+            };
+        }
+    }
+
+    const users = await db.User.findAll({
+        where: where
+    });
+
+    if (users.length ===0)
+        return {
+            code: 404,
+            message: 'No se encontraron coincidencias'
+        }
+
+    return {
+        code: 200,
+        message: users
+    };
+};
+
+const bulkCreate = async (req) => {
+    const users = req.body.users;
+    let exitosos = 0;
+    let errores = 0;
+
+    for (const user of users) {
+        const { name, email, password, password_second, cellphone } = user;
+
+        if (password !== password_second) {
+            errores++;
+            continue;
+        }
+
+        const existingUser = await db.User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (existingUser) {
+            errores++;
+            continue;
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            await db.User.create({
+                name,
+                email,
+                password: encryptedPassword,
+                cellphone,
+                status: true
+            });
+            exitosos++;
+        } catch (error) {
+            errores++;
+        }
+    }
+
+    return {
+        code: 200,
+        message: `Se crearon exitosamente ${exitosos} usuarios, usuarios con errores: ${errores} .`,
+    };
+};
+
+
 export default {
     createUser,
     getUserById,
     updateUser,
     deleteUser,
+    getAllUsers,
+    findUsers,
+    bulkCreate
 }
